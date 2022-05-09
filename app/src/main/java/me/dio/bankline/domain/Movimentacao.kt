b4b61cad4047d497fbc288b08cb6704a089c6540
Carlos Augusto D. Pacheco <carlos.pacheco@aluno.ifsp.edu.br>
2022-05-09 16:58:02 -0300

    Commit final, produto funcional mostrando as movimentacoes.

--- app/src/main/java/me/dio/bankline/domain/Movimentacao.kt
+++ app/src/main/java/me/dio/bankline/domain/Movimentacao.kt
@@ -1,11 +1,13 @@
 package me.dio.bankline.domain
 
+import com.google.gson.annotations.SerializedName
+
 data class Movimentacao(
     val id: Int,
     val dataHora: String,
     val descricao: String,
     val valor: Double,
     val tipo: TipoMovimentacao,
-    //ToDo Mapear idConta -> IdCorrentista
+    @SerializedName("idConta")
     val idCorrentista: Int
 )
